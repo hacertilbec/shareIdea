f131027bc2ebc5eaf8866402ce84a220095379f7
beyzanur kocak <beyzanurkocak@std.sehir.edu.tr>
2018-06-05 14:36:08 +0300

    conflicts resolved

--- backend/shareIdea/models.py
+++ backend/shareIdea/models.py
@@ -7,7 +7,6 @@ class userProfile(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100, blank=True, default='')
     surname = models.CharField(max_length=100, blank=True, default='')
-<<<<<<< HEAD
     department = models.CharField(max_length=100, blank=True, default='')
     statement = models.CharField(max_length=500, blank=True, default='')
     qualifications = models.CharField(max_length=500, blank=True, default='')
@@ -33,29 +32,6 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
-=======
-    school = models.CharField(max_length=100, blank=True, default='')
-    password = models.CharField(max_length=50)
-    email = models.CharField(max_length=250)
-    department = models.CharField(max_length=250)
-    personal_statement = models.CharField(max_length=500)
-    qualifications = models.CharField(max_length=500)
->>>>>>> origin/master
 
     class Meta:
         ordering = ('created',)
-
-    def __str__(self):
-        return self.name
-
-
-class Project(models.Model):
-    owner = models.ManyToManyField(userProfile)
-    title = models.CharField(max_length=250)
-    description = models.CharField(max_length=500)
-    stored_count = models.IntegerField()
-    date = models.DateTimeField(max_length=250)
-
-    def __str__(self):
-        return self.title
-
--- backend/shareIdea/urls.py
+++ backend/shareIdea/urls.py
@@ -5,13 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^userProfiles/$', views.userProfile_list),
     url(r'^userProfiles/(?P<pk>[0-9]+)/$', views.userProfile_detail),
-<<<<<<< HEAD
     url(r'^projects/$', views.project_list),
     url(r'^projects/(?P<pk>[0-9]+)/$', views.project_detail),
 
 ]
-=======
-    url(r'^projects/$', views.projects_list),
-    url(r'^projectsDetails/$', views.project_details),
-]
->>>>>>> origin/master
